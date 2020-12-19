2a1fcbcc0a7518c5b51c301f0e43fb4477aa6add
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-12-19 11:35:35 -0800

    guess a number game and compareNumbers test

---
+++ app.js
@@ -0,0 +1,48 @@
+// import functions and grab DOM elements
+import { compareNumbers } from './gameUtils.js'
+const gameBox = document.querySelector('.game-box')
+const triesRemaining = document.querySelector('.tries-remaining')
+const hint = document.querySelector('.hint')
+const usersGuess = document.querySelector('.users-guess')
+const submitButton = document.querySelector('.submit-button')
+const winResults = document.querySelector('.win-results')
+const ohNoResults = document.querySelector('.oh-no-results')
+
+// initialize state
+// choose random number 
+let number = Math.ceil(Math.random() * 20)
+
+let tries = 3;
+
+// set event listeners to update state and DOM
+submitButton.addEventListener("click", () => {
+  let usersNumber = Number(usersGuess.value)
+
+  let guessResponse = compareNumbers(usersNumber, number)
+
+  if (guessResponse === 1) {
+    hint.textContent = 'Oops, too high!'
+    tries--;
+    triesRemaining.textContent = `You have ${tries} remaining`
+
+    if (tries <= 0) {
+      ohNoResults.classList.remove("hidden")
+      gameBox.classList.add("hidden")
+    }
+
+  } else if (guessResponse === -1) {
+    hint.textContent = 'Oops, too low!'
+    tries--;
+    triesRemaining.textContent = `You have ${tries} remaining`
+
+    if (tries <= 0) {
+      ohNoResults.classList.remove("hidden")
+      gameBox.classList.add("hidden")
+    }
+
+  } else if (guessResponse === 0) {
+    console.log('you win')
+    gameBox.classList.add("hidden")
+    winResults.classList.remove("hidden")
+  }
+})
